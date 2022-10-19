ef9bb037c4214f74c899a15bd8df5d95fb2d1c58
Emanuel Ramponi <emanuel.ramponi1@gmail.com>
2022-10-18 23:37:26 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Program
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+
+            Test.Mensaje();
+            Ejercicio.Ex1();
+
+        }
+    }
+
+    public class Test
+    {
+        public static void Mensaje()
+        {
+            Console.WriteLine("Este es un mensaje");
+            Console.WriteLine("Este es otro mensaje");
+        }
+                
+    }
+
+    public class Ejercicio
+    {
+        public static void Ex1()
+        {
+            Console.Write("Nombre: ");
+            string nombre = Console.ReadLine();
+            Console.Write("Apellido: ");
+            string apellido = Console.ReadLine();
+            Console.Write("Edad: ");
+            string edad = Console.ReadLine();
+            Console.Write("Aficion: ");
+            string aficion = Console.ReadLine();
+
+            Console.WriteLine("============= INFORMACION BASICA =============");
+            Console.WriteLine($"Eres {nombre} {apellido}");
+            Console.WriteLine($"Tienes {edad} años de edad");
+            Console.WriteLine($"Hobby: {aficion}");
+
+        }
+    }
+
+
+}
